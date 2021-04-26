2e9602ff083540e4ad839b74b7e6d51feaa18fc1
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2021-04-26 14:19:52 +0530

    the all stuff

---
+++ paper.js
@@ -0,0 +1,41 @@
+class Paper{
+
+	constructor (x, y){
+
+	 var options={
+		 'density' : 1.5,
+		 'friction' : 1.0,
+		 'restitution' : 0.2,
+		 'isStatic' : false
+	 }
+		this.x = x;
+		this.y = y;
+		this.Width = 60;
+		this.Height = 60;
+		
+		
+		this.image=loadImage("paper.png")
+		this.body=Bodies.rectangle(this.x, this.y, this.Width, this.Height, options)
+		World.add(world, this.body)
+		
+
+	}
+	display()
+	{        
+		    
+
+			var pos_y = this.body.position.y;
+			var pos_x = this.body.position.x;
+			var angle = this.body.angle;
+
+			push()
+			translate(pos_x , pos_y);
+			rotate(angle)
+		    imageMode(CENTER)
+			
+			image(this.image, 0, 0, this.Width, this.Height)
+			pop()
+			
+	}
+
+}
